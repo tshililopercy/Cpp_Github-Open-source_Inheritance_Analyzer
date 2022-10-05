50e14ef099368507c9716a62e09af5dd99d5f455
Ralukake Tshililo <chililopercy7@gmail.com>
2022-10-06 00:53:25 +0200

    modify github clone module

--- GitHub_Search_And_Clone.py
+++ GitHub_Search_And_Clone.py
@@ -2,9 +2,12 @@ import requests
 from git import Repo
 import os
 
-
 def Clone(Repository_URL):
-   Repo.clone_from(Repository_URL, "Repository", depth = 1)
-   
+   Response = requests.get(Repository_URL)
+   if Response.status_code == 200:
+        Repo.clone_from(Repository_URL, "../Repository", depth = 1)
+   else:
+       print("Invalid Repo URL")
 if __name__ == '__main__':
-    Clone("https://github.com/grim-firefly/C-CPP-Project.git")
+    Clone("https://github.com/Frkncln/Cpp-Inheritance-.git")
+    

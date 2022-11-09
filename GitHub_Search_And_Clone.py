14d98e6b3055038c29d25dc99b8447edaebd304e
Ralukake Tshililo <chililopercy7@gmail.com>
2022-11-09 18:02:36 +0200

    Added Multiple Root duplicate solution, getting 100 repositories with most stars

--- GitHub_Search_And_Clone.py
+++ GitHub_Search_And_Clone.py
@@ -4,8 +4,7 @@ import os
 
 class Cloner:
     def __init__(self):
-        #rmtree("../Repository")
-        response = requests.get('https://api.github.com/search/repositories?q=language:Cpp')
+        response = requests.get('https://api.github.com/search/repositories?q=language:Cpp&&per_page=100&page='+ str(1))
         self.repos_info = {}
         if response.status_code == 200:
             response_dict = response.json()
@@ -29,20 +28,3 @@ class Cloner:
 
 if __name__ == '__main__':
     cloner = Cloner()
-    # repos_info = cloner.get_repos_info()
-    # for repo in repos_info:
-    #     # clear text
-    #     # print clone status
-    #     print_str = ""
-    #     before_current = True
-    #     for repo2 in repos_info:
-    #         if (repo.name == repo2.name):
-    #             print_str += "Repo: " + repo.name + " cloning...."
-    #             before_current = False
-    #         elif before_current:
-    #             print_str += "Repo: " + repo.name + " clone complete"
-    #         else:
-    #             print_str += "Repo: " + repo.name + " waiting...."
-    #         cloner.cloneARepo(repo)
-        
-    #     # setText(print_str)  
